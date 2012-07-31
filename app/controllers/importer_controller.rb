c5cee164e359161f4cb12e287dd3a544d333c9cc
ichizok <gclient.gaap@gmail.com>
2012-07-31 15:33:36 +0900

    refactoring, modified views

--- app/controllers/importer_controller.rb
+++ app/controllers/importer_controller.rb
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-require 'tempfile'
-require 'nkf'
 
 class ImporterController < ApplicationController
   unloadable
@@ -8,9 +6,11 @@ class ImporterController < ApplicationController
   before_filter :find_project
   before_filter :authorize,:except => :result
 
-  ISSUE_ATTRS = [:id, :subject, :parent_issue, :assigned_to, :fixed_version,
-    :author, :description, :category, :priority, :tracker, :status,
-    :start_date, :due_date, :done_ratio, :estimated_hours]
+  ISSUE_ATTRS = [
+    :id, :subject, :parent_issue, :assigned_to, :fixed_version, :author,
+    :description, :category, :priority, :tracker, :status, :start_date,
+    :due_date, :done_ratio, :estimated_hours
+  ]
 
   def index
   end
@@ -22,240 +22,201 @@ class ImporterController < ApplicationController
     wrapper = params[:wrapper]
     encoding = params[:encoding]
 
-    if file == nil
-      flash[:error] = l(:label_file_undefined);
-      redirect_to :action => "index", :project_id => params[:project_id]
+    if file.nil?
+      redirect_to({:action => "index", :project_id => params[:project_id]}, :notice => l(:label_file_undefined))
       return
     end
 
     # save import file
     @original_filename = file.original_filename
-    tmpfile = Tempfile.new("redmine_importer")
-    if tmpfile
-      tmpfile.write(convert_file(file, encoding))
-      tmpfile.close
-      tmpfilename = File.basename(tmpfile.path)
-      if !$tmpfiles
-        $tmpfiles = Hash.new
-      end
-      $tmpfiles[tmpfilename] = tmpfile
-    else
-      flash[:error] = "Cannot save import file."
-      return
-    end
+    tmpfilepath = file.tempfile.path
 
-    session[:importer_tmpfile] = tmpfilename
+    session[:importer_tmpfile] = tmpfilepath
     session[:importer_splitter] = splitter
     session[:importer_wrapper] = wrapper
     session[:importer_encoding] = encoding
 
     # display sample
     sample_count = 5
-    i = 0
     @samples = []
 
-    CSV.foreach(tmpfile.path, {:headers=>true, :encoding=>"UTF-8", :quote_char=>wrapper, :col_sep=>splitter}) do |row|
-      @samples[i] = row
-
-      i += 1
-      if i >= sample_count
-        break
-      end
-    end # do
-
-    if @samples.size > 0
-      @headers = @samples[0].headers
+    begin
+      CSV.open(tmpfilepath, {:headers => true, :encoding => encoding, :quote_char => wrapper, :col_sep => splitter}) do |csv|
+        sample_count.times do
+          row = csv.shift
+          @samples << row if row
+        end
+        @headers = csv.headers
+      end # do
+    rescue => err
+      redirect_to({:action => "index", :project_id => params[:project_id]}, :notice => "Failed to read file (#{err.message})")
+      return
     end
- 
-    @headers.each { |h|
+
+    @headers.each do |h|
       if h.blank?
-        flash[:error] = l(:label_header_blank);
-        redirect_to :action => "index", :project_id => params[:project_id]
+        redirect_to({:action => "index", :project_id => params[:project_id]}, :notice => l(:label_header_blank))
         return
       end
-    }
+    end
 
     # fields
-    @attrs = Array.new
+    @attrs = []
+
     ISSUE_ATTRS.each do |attr|
       @attrs.push([l("field_#{attr}".to_sym), attr])
     end
+
     @project.all_issue_custom_fields.each do |cfield|
       @attrs.push([cfield.name, cfield.name])
     end
+
     @attrs.sort!
-     
   end
 
   def result
-    tmpfilename = session[:importer_tmpfile]
+    tmpfilepath = session[:importer_tmpfile]
     splitter = session[:importer_splitter]
     wrapper = session[:importer_wrapper]
     encoding = session[:importer_encoding]
-    
-    if tmpfilename
-      tmpfile = $tmpfiles[tmpfilename]
-      if tmpfile == nil
-        flash[:error] = "Missing imported file"
-        return
-      end
+
+    if !File.exist?(tmpfilepath)
+      redirect_to({:action => "index", :project_id => params[:project_id]}, :notice => "Missing imported file")
+      return
     end
-    
+
     default_tracker = params[:default_tracker]
     update_issue = params[:update_issue]
     unique_field = params[:unique_field]
     journal_field = params[:journal_field]
     update_other_project = params[:update_other_project]
     ignore_non_exist = params[:ignore_non_exist]
-    fields_map = params[:fields_map]
+    fields_map = params[:fields_map].inject({}) do |h, (k, v)|
+      h[k.dup.force_encoding('UTF-8')] = v; h
+    end
     unique_attr = fields_map[unique_field]
 
     # check params
-    if update_issue && unique_attr == nil
-      flash[:error] = "Unique field hasn't match an issue's field"
+    if update_issue && unique_attr.nil?
+      redirect_to({:action => "index", :project_id => params[:project_id]}, :notice => "Unique field hasn't match an issue's field")
       return
     end
 
     @handle_count = 0
     @update_count = 0
-    @skip_count = 0
     @failed_count = 0
-    @failed_issues = Hash.new
-    @affect_projects_issues = Hash.new
+    @failed_issues = {}
+    @affect_projects_issues = Hash.new(0)
 
     # attrs_map is fields_map's invert
     attrs_map = fields_map.invert
 
-    CSV.foreach(tmpfile.path, {:headers=>true, :encoding=>'UTF-8', :quote_char=>wrapper, :col_sep=>splitter}) do |row|
+    CSV.open(tmpfilepath, {:headers => true, :encoding => encoding, :quote_char => wrapper, :col_sep => splitter}) do |csv|
+      csv.each do |row|
 
-      project = Project.find_by_name(row[attrs_map["project"]])
-      tracker = Tracker.find_by_name(row[attrs_map["tracker"]])
-      status = IssueStatus.find_by_name(row[attrs_map["status"]])
-      author = User.find_by_login(row[attrs_map["author"]])
-      priority = Enumeration.find_by_name(row[attrs_map["priority"]])
-      category = IssueCategory.find_by_name(row[attrs_map["category"]])
-      assigned_to = User.find_by_login(row[attrs_map["assigned_to"]])
-  
-      # new issue or find exists one
-      issue = Issue.new
-      journal = nil
-      issue.project_id = project != nil ? project.id : @project.id
-      issue.tracker_id = tracker != nil ? tracker.id : default_tracker
-      issue.author_id = author != nil && author.class.name != "AnonymousUser" ? author.id : User.current.id
-      fixed_version = Version.find_by_name_and_project_id(row[attrs_map["fixed_version"]], issue.project_id)
+        project = Project.find_by_name(row[attrs_map["project"]])
+        tracker = Tracker.find_by_name(row[attrs_map["tracker"]])
+        status = IssueStatus.find_by_name(row[attrs_map["status"]])
+        author = User.find_by_login(row[attrs_map["author"]])
+        priority = Enumeration.find_by_name(row[attrs_map["priority"]])
+        category = IssueCategory.find_by_name(row[attrs_map["category"]])
+        assigned_to = User.find_by_login(row[attrs_map["assigned_to"]])
 
-      if update_issue
-        # custom field
-        if !ISSUE_ATTRS.include?(unique_attr.to_sym)
-          issue.available_custom_fields.each do |cf|
-            if cf.name == unique_attr
-              unique_attr = "cf_#{cf.id}"
-              break
-            end
-          end 
-        end
-        
-        if unique_attr == "id"
-          issues = [Issue.find_by_id(row[unique_field])]
-        else
-          query = Query.new(:name => "_importer", :project => @project)
-          query.add_filter("status_id", "*", [1])
-          query.add_filter(unique_attr, "=", [row[unique_field]])
+        # new issue or find exists one
+        issue = Issue.new
+        journal = nil
+        issue.project_id = project ? project.id : @project.id
+        issue.tracker_id = tracker ? tracker.id : default_tracker
+        issue.author_id = author && author.class.name != "AnonymousUser" ? author.id : User.current.id
+        fixed_version = Version.find_by_name_and_project_id(row[attrs_map["fixed_version"]], issue.project_id)
 
-          issues = Issue.find :all, :conditions => query.statement, :limit => 2, :include => [ :assigned_to, :status, :tracker, :project, :priority, :category, :fixed_version ]
-        end
-        
-        if issues.size > 1
-          flash[:warning] = "Unique field #{unique_field} has duplicate record"
-          @failed_count += 1
-          @failed_issues[@handle_count + 1] = row
-          break
-        else
-          if issues.size > 0
-            # found issue
-            issue = issues.first
-            
-            # ignore other project's issue or not
-            if issue.project_id != @project.id && !update_other_project
-              @skip_count += 1
-              next              
-            end
-            
-            # ignore closed issue except reopen
-            if issue.status.is_closed?
-              if status == nil || status.is_closed?
-                @skip_count += 1
-                next
+        if update_issue
+          # custom field
+          if !ISSUE_ATTRS.include?(unique_attr.to_sym)
+            issue.available_custom_fields.each do |cf|
+              if cf.name == unique_attr
+                unique_attr = "cf_#{cf.id}"
+                break
               end
             end
-            
-            # init journal
-            note = row[journal_field] || ''
-            journal = issue.init_journal(author || User.current, 
-              note || '')
-              
-            @update_count += 1
+          end
+
+          if unique_attr == "id"
+            issues = [Issue.find_by_id(row[unique_field])]
           else
-            # ignore none exist issues
-            if ignore_non_exist
-              @skip_count += 1
-              next
+            query = Query.new(:name => "_importer", :project => @project)
+            query.add_filter("status_id", "*", [1])
+            query.add_filter(unique_attr, "=", [row[unique_field]])
+
+            issues = Issue.find :all, :conditions => query.statement, :limit => 2, :include => [ :assigned_to, :status, :tracker, :project, :priority, :category, :fixed_version ]
+          end
+
+          if issues.size > 1
+            flash[:warning] = "Unique field #{unique_field} has duplicate record"
+            @failed_issues[@handle_count + 1] = row
+            break
+          else
+            if issues.size > 0
+              # found issue
+              issue = issues.first
+
+              # ignore other project's issue or not
+              next if issue.project_id != @project.id && !update_other_project
+
+              # ignore closed issue except reopen
+              next if issue.status.is_closed? && (status.nil? || status.is_closed?)
+
+              # init journal
+              note = row[journal_field] || ''
+              journal = issue.init_journal(author || User.current, note || '')
+
+              @update_count += 1
+            else
+              # ignore none exist issues
+              next if ignore_non_exist
             end
           end
         end
-      end
-      
-      # project affect
-      if project == nil
-        project = Project.find_by_id(issue.project_id)
-      end
-      @affect_projects_issues.has_key?(project.name) ?
-        @affect_projects_issues[project.name] += 1 : @affect_projects_issues[project.name] = 1
 
-      # required attributes
-      issue.status_id = status != nil ? status.id : issue.status_id
-      issue.priority_id = priority != nil ? priority.id : issue.priority_id
-      issue.subject = row[attrs_map["subject"]] || issue.subject
-      
-      # optional attributes
-      issue.parent_issue_id = row[attrs_map["parent_issue"]] || issue.parent_issue_id
-      issue.description = row[attrs_map["description"]] || issue.description
-      issue.category_id = category != nil ? category.id : issue.category_id
-      issue.start_date = row[attrs_map["start_date"]] || issue.start_date
-      issue.due_date = row[attrs_map["due_date"]] || issue.due_date
-      issue.assigned_to_id = assigned_to != nil && assigned_to.class.name != "AnonymousUser"? assigned_to.id : issue.assigned_to_id
-      issue.fixed_version_id = fixed_version != nil ? fixed_version.id : issue.fixed_version_id
-      issue.done_ratio = row[attrs_map["done_ratio"]] || issue.done_ratio
-      issue.estimated_hours = row[attrs_map["estimated_hours"]] || issue.estimated_hours
+        # project affect
+        project = Project.find_by_id(issue.project_id) if project.nil?
 
-      # custom fields
-      issue.custom_field_values = issue.available_custom_fields.inject({}) do |h, c|
-        if value = row[attrs_map[c.name]]
-          h[c.id] = value
+        @affect_projects_issues[project.name] += 1
+
+        # required attributes
+        issue.status_id = status ? status.id : issue.status_id
+        issue.priority_id = priority ? priority.id : issue.priority_id
+        issue.subject = row[attrs_map["subject"]] || issue.subject
+
+        # optional attributes
+        issue.parent_issue_id = row[attrs_map["parent_issue"]] || issue.parent_issue_id
+        issue.description = row[attrs_map["description"]] || issue.description
+        issue.category_id = category ? category.id : issue.category_id
+        issue.start_date = row[attrs_map["start_date"]] || issue.start_date
+        issue.due_date = row[attrs_map["due_date"]] || issue.due_date
+        issue.assigned_to_id = assigned_to && assigned_to.class.name != "AnonymousUser"? assigned_to.id : issue.assigned_to_id
+        issue.fixed_version_id = fixed_version ? fixed_version.id : issue.fixed_version_id
+        issue.done_ratio = row[attrs_map["done_ratio"]] || issue.done_ratio
+        issue.estimated_hours = row[attrs_map["estimated_hours"]] || issue.estimated_hours
+
+        # custom fields
+        issue.custom_field_values = issue.available_custom_fields.inject({}) do |h, cf|
+          value = row[attrs_map[cf.name]]
+          h[cf.id] = value if value; h
         end
-        h
+
+        @failed_issues[@handle_count + 1] = row if !issue.save
+
+        @handle_count += 1
       end
 
-      if (!issue.save)
-        # 记录错误
-        @failed_count += 1
-        @failed_issues[@handle_count + 1] = row
-      end
-  
-      if journal
-        journal
-      end
-      
-      @handle_count += 1
+      @headers = csv.headers
     end # do
-    
-    if @failed_issues.size > 0
-      @failed_issues = @failed_issues.sort
-      @headers = @failed_issues[0][1].headers
-    end
+
+    @failed_count = @failed_issues.count
   end
 
-private
+  private
 
   def find_project
     begin
@@ -264,20 +225,5 @@ private
       render_404
     end
   end
-
-#  def convert_headers_encoding(headers, encoding)
-#    nkf_option = nil
-#    nkf_option = '-Sw' if encoding == 'S'
-#    nkf_option = '-Ew' if encoding == 'EUC'
-#    nkf_option ? headers.map! {|e| NKF.nkf('-m0 -x ' + nkf_option, e)} : headers
-#  end
-
-  def convert_file(file, encoding)
-    nkf_option = nil
-    nkf_option = '-Sw' if encoding == 'S'
-    nkf_option = '-Ew' if encoding == 'EUC'
-    nkf_option ? NKF.nkf('-m0 -x ' + nkf_option, file.read) : file.read 
-  end
-
 
 end
