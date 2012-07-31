a780eed944eac9bc414c025078b1446393e663b6
ichizok <gclient.gaap@gmail.com>
2012-07-31 17:42:30 +0900

    modified routes, menu position

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedmineApp::Application.routes.draw do
-  match 'projects/:project_id/importer', :to => 'importer#index'
-  match 'projects/:project_id/importer/:action', :to => 'importer'
+  match 'projects/:project_id/issues/import', :to => 'importer#index'
+  match 'projects/:project_id/issues/import/:action', :to => 'importer'
 end
--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_importer do
-  name 'Redmine Issue Importer'
+  name 'Redmine Issue Importer plugin'
   author 'Martin Liu'
   description 'Issue import plugin for Redmine.'
   version '0.4.0'
@@ -12,5 +12,5 @@ Redmine::Plugin.register :redmine_importer do
     permission :import, {:importer => [:index, :match, :result]}, :require => :member
   end
 
-  menu :project_menu, :importer, { :controller => 'importer', :action => 'index' }, :caption => :label_import, :before => :settings, :param => :project_id
+  menu :project_menu, :importer, { :controller => 'importer', :action => 'index' }, :caption => :label_import, :after => :new_issue, :param => :project_id
 end
