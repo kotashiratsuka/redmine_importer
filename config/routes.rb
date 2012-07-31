5fe572ade7dd1b40280a24de7595ef90bd973ad8
ichizok <gclient.gaap@gmail.com>
2012-07-31 15:32:51 +0900

    set routes compatible with Rails3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:project_id/importer/:action', :controller => 'importer'
+RedmineApp::Application.routes.draw do
+  match 'projects/:project_id/importer', :to => 'importer#index'
+  match 'projects/:project_id/importer/:action', :to => 'importer'
 end
--- init.rb
+++ init.rb
@@ -1,19 +1,16 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_importer do
-  name 'Issue Importer'
+  name 'Redmine Issue Importer'
   author 'Martin Liu'
   description 'Issue import plugin for Redmine.'
-  version '0.3.1'
+  version '0.4.0'
+  requires_redmine :version_or_higher => '2.0.0'
+  url 'https://github.com/farend/redmine_importer'
 
   project_module :importer do
-    permission :import, :importer => :index
-    permission :match, :importer => :match
-   
+    permission :import, {:importer => [:index, :match, :result]}, :require => :member
   end
-  menu :project_menu, :importer, { :controller => 'importer', :action => 'index' }, :caption => :label_import, :before => :settings, :param => :project_id
 
-  ActionController::Routing::Routes.draw do |map|
-    map.connect 'projects/:project_id/importer/:action', :controller => 'importer'
-  end
+  menu :project_menu, :importer, { :controller => 'importer', :action => 'index' }, :caption => :label_import, :before => :settings, :param => :project_id
 end
