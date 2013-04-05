44f7a6d427a5ad32419f0ea43fec7378d5964b2f
ichizok <gclient.gaap@gmail.com>
2013-04-05 12:31:39 +0900

    adjusted localed text

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_importer do
   description 'Issue import plugin for Redmine.'
   version '0.4.0'
   requires_redmine :version_or_higher => '2.0.0'
-  url 'https://github.com/farend/redmine_importer'
+  url 'https://github.com/ichizok/redmine_importer'
 
   project_module :importer do
     permission :import, {:importer => [:index, :match, :result]}, :require => :member
